4e2ab92671ade6e8971485dd82d2f4d1fd6d92a6
Johan Nordström <johan@digitalvillage.se>
2026-03-02 10:36:05 +0100

    Test that we can add any two numbers.

--- 01-unit-testing/src/calc.test.ts
+++ 01-unit-testing/src/calc.test.ts
@@ -4,3 +4,8 @@ import { add } from "./calc";
 it("should add 1 + 2", () => {
 	expect(add(1, 2)).toBe(3);
 });
+
+it("should add any two numbers", () => {
+	expect(add(3, 4)).toBe(7);
+	expect(add(13, 7)).toBe(20);
+});
