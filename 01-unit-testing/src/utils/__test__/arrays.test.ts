dc15a7482b78841f9eb4b4159f192ee840c03ed2
Johan Nordström <johan@digitalvillage.se>
2026-03-02 14:07:59 +0100

    Dumb demo of `beforeAll()`, because I have no imagination.

--- 01-unit-testing/src/utils/__test__/arrays.test.ts
+++ 01-unit-testing/src/utils/__test__/arrays.test.ts
@@ -1,12 +1,17 @@
-import { beforeEach, describe, expect, it } from "vitest";
+import { beforeAll, beforeEach, describe, expect, it } from "vitest";
 import { clone } from "../arrays";
 
 describe("clones an array", () => {
 	const a = ["i", "like", "unit", "tests"];
 	let b: any[] = [];
 
-	beforeEach(() => {
+	// is called before **all** (once) the tests in this suite is executed
+	beforeAll(() => {
 		b = clone(a);
+	});
+
+	// is called before **every** (one time per test) test in this suite is executed
+	beforeEach(() => {
 	});
 
 	it("contains the same number of items", () => {
