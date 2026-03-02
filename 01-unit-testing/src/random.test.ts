c6e53c18d04ffcdbc4804bde5814406170e22507
Johan Nordström <johan@digitalvillage.se>
2026-03-02 11:00:04 +0100

    Workshop: Tests for random number generator.

---
+++ 01-unit-testing/src/random.test.ts
@@ -0,0 +1,11 @@
+import { expect, it } from "vitest";
+import { getRandomNumber } from "./random";
+
+it.todo("generates a random number between 1-10", () => {
+});
+
+it.todo("generates a random number between 1-50", () => {
+});
+
+it.todo("generates 10 random numbers between 1-20", () => {
+});
---
+++ 01-unit-testing/src/random.ts
@@ -0,0 +1,6 @@
+/**
+ * Returns a random number between 1-10
+ */
+export const getRandomNumber = () => {
+	return Math.floor(Math.random() * 10) + 1;
+}
