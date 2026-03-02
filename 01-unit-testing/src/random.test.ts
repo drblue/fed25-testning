4c8969e9b9f94ea8ad5ce046f125b81a42c63023
Johan Nordström <johan@digitalvillage.se>
2026-03-02 11:53:53 +0100

    ✅ Tests for random number generator.

--- 01-unit-testing/src/random.test.ts
+++ 01-unit-testing/src/random.test.ts
@@ -1,11 +1,37 @@
 import { expect, it } from "vitest";
-import { getRandomNumber } from "./random";
+import { getRandomNumber, getRandomNumbers } from "./random";
 
-it.todo("generates a random number between 1-10", () => {
+it("generates a random number between 1-10", () => {
+	const randomNumber = getRandomNumber();
+
+	expect(randomNumber).toBeGreaterThanOrEqual(1);
+	expect(randomNumber).toBeLessThanOrEqual(10);
 });
 
-it.todo("generates a random number between 1-50", () => {
+it("generates a random number between 1-50", () => {
+	const randomNumber = getRandomNumber(50);
+
+	expect(randomNumber).toBeGreaterThanOrEqual(1);
+	expect(randomNumber).toBeLessThanOrEqual(50);
 });
 
-it.todo("generates 10 random numbers between 1-20", () => {
+it("generates random numbers between 1-20, 10 times", () => {
+	const max = 20;
+
+	for (let i = 0; i < 10; i++) {
+		const randomNumber = getRandomNumber(max);
+
+		expect(randomNumber).toBeGreaterThanOrEqual(1);
+		expect(randomNumber).toBeLessThanOrEqual(max);
+	}
+});
+
+it("generates 10 random numbers between 1-20", () => {
+	const max = 20;
+	const randomNumbers = getRandomNumbers(10, max);
+
+	randomNumbers.forEach(randomNumber => {
+		expect(randomNumber).toBeGreaterThanOrEqual(1);
+		expect(randomNumber).toBeLessThanOrEqual(max)
+	});
 });
--- 01-unit-testing/src/random.ts
+++ 01-unit-testing/src/random.ts
@@ -1,6 +1,19 @@
 /**
- * Returns a random number between 1-10
+ * Returns a random number between 1..max (default 10)
  */
-export const getRandomNumber = () => {
-	return Math.floor(Math.random() * 10) + 1;
+export const getRandomNumber = (max = 10) => {
+	return Math.floor(Math.random() * max) + 1;
+}
+
+/**
+ * Returns qty amount of random numbers between 1..max
+ */
+export const getRandomNumbers = (qty: number, max = 10) => {
+	const randomNumbers = [];
+
+	for (let i = 0; i < qty; i++) {
+		randomNumbers.push(getRandomNumber(max));
+	}
+
+	return randomNumbers;
 }
