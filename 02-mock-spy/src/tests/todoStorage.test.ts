b877e53f8003a5c4ff4b5ddecc82276137073c55
Johan Nordström <johan@digitalvillage.se>
2026-03-04 11:47:18 +0100

    🕵🏻‍♂️ Spy on `getItem` and `setItem` from `localStorage`-mock.

--- 02-mock-spy/src/tests/todoStorage.test.ts
+++ 02-mock-spy/src/tests/todoStorage.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { getMockedLocalStorage } from "../mocks/mockedLocalStorage";
 import type { Todo } from "../types/Todo";
 import { getTodos, saveTodos } from "../utils/todoStorage";
@@ -27,23 +27,30 @@ afterEach(() => {
 
 describe("get todos", () => {
 	it("returns empty list of todos", () => {
+		// 🕵🏻‍♂️ register a spy on `localStorage.getItem`
+		const getItemSpy = vi.spyOn(globalThis.localStorage, "getItem");
 		const todos = getTodos();
 
+		// 🔍 make sure that `localStorage.getItem` has been called once
+		expect(getItemSpy).toHaveBeenCalledOnce();
 		expect(todos).toHaveLength(0);
 	});
 });
 
 describe("save todos", () => {
 	it("can save a todo", () => {
+		// 🕵🏻‍♂️ register a spy on `localStorage.setItem`
+		const setItemSpy = vi.spyOn(globalThis.localStorage, "setItem");
+
 		// save TODO and sure it's ok
-		// i.e. test `saveTodos`
 		const res = saveTodos([ TODO ]);
+
+		expect(setItemSpy).toHaveBeenCalledOnce();
 		expect(res.success).toBe(true);
 	});
 
 	it("can save a todo and then retrieve it", () => {
 		// save TODO and then make sure we can retrieve it
-		// i.e. test `saveTodos` and check that the saved todo exists when invoking `getTodos`
 		const res = saveTodos([ TODO ]);
 		expect(res.success).toBe(true);
 
