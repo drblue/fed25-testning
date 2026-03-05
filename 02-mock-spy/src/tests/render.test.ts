14f0e16c9a7c1c1f8458e3157c63f5b3ceb04b5b
Johan Nordström <johan@digitalvillage.se>
2026-03-05 10:31:32 +0100

    Test that we can render a single todo.

--- 02-mock-spy/src/tests/render.test.ts
+++ 02-mock-spy/src/tests/render.test.ts
@@ -1,5 +1,9 @@
+/**
+ * @vitest-environment happy-dom
+ */
 import { describe, expect, it } from "vitest";
 import { transformTodosToHtml } from "../utils/render";
+import testTodos from "./data/todos";
 
 describe("transform todos", () => {
 	it("should output empty list when no todos exist", () => {
@@ -7,5 +11,12 @@ describe("transform todos", () => {
 		expect(lis).toBe("");
 	});
 
-	it.todo("should output list with one todo", () => {});
+	it("should output list with one todo", () => {
+		const lis = transformTodosToHtml([ testTodos[0] ]);
+		document.body.innerHTML = `<ul id="#todos">${lis}</ul>`;
+
+		// query dom for any listitems with the `todo` class
+		const liEls = document.querySelectorAll("li.todo");
+		expect(liEls).toHaveLength(1);
+	});
 });
