78c912295e57460d4ea30856b025ce46677df4ec
Johan Nordström <johan@digitalvillage.se>
2026-03-05 10:42:42 +0100

    Improved test readability by writing a helper function.

--- 02-mock-spy/src/tests/render.test.ts
+++ 02-mock-spy/src/tests/render.test.ts
@@ -4,6 +4,19 @@
 import { describe, expect, it } from "vitest";
 import { transformTodosToHtml } from "../utils/render";
 import testTodos from "./data/todos";
+import type { Todo } from "../types/Todo";
+
+/**
+ * Helper function for rendering todos to DOM
+ *
+ * @param todos Array of todos
+ * @returns Reference to UL-element in DOM
+ */
+const renderTodosToDOM = (todos: Todo[]) => {
+	const lis = transformTodosToHtml(todos);
+	document.body.innerHTML = `<ul id="todos">${lis}</ul>`;
+	return document.querySelector<HTMLUListElement>("#todos")!;
+}
 
 describe("transform todos", () => {
 	it("should output empty list when no todos exist", () => {
@@ -12,11 +25,10 @@ describe("transform todos", () => {
 	});
 
 	it("should output list with one todo", () => {
-		const lis = transformTodosToHtml([ testTodos[0] ]);
-		document.body.innerHTML = `<ul id="#todos">${lis}</ul>`;
+		const ulEl = renderTodosToDOM([ testTodos[0] ]);
 
-		// query dom for any listitems with the `todo` class
-		const liEls = document.querySelectorAll("li.todo");
+		// query UL for any listitems with the `todo` class
+		const liEls = ulEl.querySelectorAll("li.todo");
 		expect(liEls).toHaveLength(1);
 	});
 });
