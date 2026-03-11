52dd20dc97e81bde73cbd7b3c3152862a37ae6ad
Johan Nordström <johan@digitalvillage.se>
2026-03-11 23:10:08 +0100

    Group tests using contexts.

--- 04-cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
+++ 04-cypress-testing-course-app/cypress/e2e/subscribe.cy.ts
@@ -13,39 +13,43 @@ describe("Newsletter Subscribe Form", () => {
 		cy.get("#__next").should("be.visible");
 	});
 
-	it("allows users to subscribe to the email list", () => {
-		// ✍🏻
-		cy.getByDataTest("email-input")
-			.should("be.visible")
-			.type(email);
+	context("Happy paths", () => {
+		it("allows users to subscribe to the email list", () => {
+			// ✍🏻
+			cy.getByDataTest("email-input")
+				.should("be.visible")
+				.type(email);
 
-		// 👆🏻
-		cy.getByDataTest("submit-button").click();
+			// 👆🏻
+			cy.getByDataTest("submit-button").click();
 
-		// 🥳❔
-		cy.getByDataTest("success-message")
-			.should("exist")
-			.contains(email);
+			// 🥳❔
+			cy.getByDataTest("success-message")
+				.should("exist")
+				.contains(email);
+		});
 	});
 
-	it("displays an error message when the email is invalid", () => {
-		cy.getByDataTest("email-input").type(invalidEmail);
-		cy.getByDataTest("submit-button").click();
-		cy.getByDataTest("success-message").should("not.exist");
-	});
+	context("Sad paths", () => {
+		it("displays an error message when the email is invalid", () => {
+			cy.getByDataTest("email-input").type(invalidEmail);
+			cy.getByDataTest("submit-button").click();
+			cy.getByDataTest("success-message").should("not.exist");
+		});
 
-	it("should not allow users to subscribe twice", () => {
-		cy.getByDataTest("email-input").type(existingSubscriberEmail);
-		cy.getByDataTest("submit-button").click();
-		cy.getByDataTest("server-error-message")
-			.should("be.visible")
-			.contains(existingSubscriberEmail);
-	});
+		it("should not allow users to subscribe twice", () => {
+			cy.getByDataTest("email-input").type(existingSubscriberEmail);
+			cy.getByDataTest("submit-button").click();
+			cy.getByDataTest("server-error-message")
+				.should("be.visible")
+				.contains(existingSubscriberEmail);
+		});
 
-	it("should not allow subscribing without an email address", () => {
-		cy.getByDataTest("submit-button").click();
-		cy.getByDataTest("error-message")
-			.should("be.visible")
-			.contains("Email is required");
+		it("should not allow subscribing without an email address", () => {
+			cy.getByDataTest("submit-button").click();
+			cy.getByDataTest("error-message")
+				.should("be.visible")
+				.contains("Email is required");
+		});
 	});
 });
