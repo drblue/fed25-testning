1853d5eeeeb8279d136b3b3cc01ce1230656c697
Johan Nordström <johan@digitalvillage.se>
2026-03-11 22:27:03 +0100

    Test that we still get 404 when trying to get a todo that does not exist (was already working).

--- 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
+++ 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
@@ -114,7 +114,7 @@ describe("TodoAPI", () => {
 		expect(todos).not.toContainEqual(createdTodo);
 	});
 
-	it.skip("should throw an error when trying to get a todo that does not exist", async () => {
+	it("should throw an error when trying to get a todo that does not exist", async () => {
 		// expect an error to be thrown
 		await expect(
 			TodoAPI.getTodo(99999)
