c822f1153df52b836690ca2be8794607dfe0de50
Johan Nordström <johan@digitalvillage.se>
2026-03-11 22:57:38 +0100

    Created our first E2E-test 🤩.

---
+++ 04-cypress-testing-course-app/cypress/e2e/home.cy.ts
@@ -0,0 +1,10 @@
+describe("template spec", () => {
+	it("the h1 contains the correct text", () => {
+		cy.visit("http://localhost:3000");
+
+		// Wait for the app to be fully hydrated and visible 💦
+		cy.get("#__next").should("be.visible");
+
+		cy.get("h1").contains("Testing Next.js Applications with Cypress");
+	});
+});
