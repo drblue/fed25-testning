6d56a51bed95b59fe5b79404ebe7ec5d25ed806b
Johan Nordström <johan@digitalvillage.se>
2026-03-15 15:51:28 +0100

    Added mini-workshop for testing of create todo.

--- 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
+++ 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
@@ -10,4 +10,14 @@ describe("Todos", () => {
 	it("should find at least one todo", () => {
 		cy.get("#todos").find("li").should("have.length.at.least", 1);
 	});
+
+	context("Create todo", () => {
+		it.skip("create todo form should be empty");
+
+		it.skip("can't create a todo without a title");
+
+		it.skip("can create a new todo, input-field is cleared and todo appears in the list");
+
+		it.skip("can type in the 'create todo' form and then reset the form");
+	});
 });
