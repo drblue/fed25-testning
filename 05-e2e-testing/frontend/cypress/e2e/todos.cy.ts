c2ebe55a2a7bc356958330796abd31f2c3004272
Johan Nordström <johan@digitalvillage.se>
2026-03-15 15:47:40 +0100

    Test that we should find at least one todo.

--- 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
+++ 05-e2e-testing/frontend/cypress/e2e/todos.cy.ts
@@ -1,5 +1,13 @@
 describe("Todos", () => {
-	it("should be able to visit the page", () => {
+	beforeEach(() => {
 		cy.visit("/");
+	});
+
+	it("should be able to visit the page", () => {
+		cy.get("h1").contains("Todos");
+	});
+
+	it("should find at least one todo", () => {
+		cy.get("#todos").find("li").should("have.length.at.least", 1);
 	});
 });
