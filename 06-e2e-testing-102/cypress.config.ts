2f226758ba64e1409cbfe9d58fcf78992ce15175
Johan Nordström <johan@digitalvillage.se>
2026-03-19 09:24:57 +0100

    Test: Can't access protected routes without authenticating first.

--- 06-e2e-testing-102/cypress.config.ts
+++ 06-e2e-testing-102/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
 	e2e: {
-		baseUrl: "https://fed-firebase-todos.netlify.app/",
+		baseUrl: "https://fed-firebase-todos.netlify.app",
 		// setupNodeEvents(on, config) {
 		// 	// implement node event listeners here
 		// },
--- 06-e2e-testing-102/cypress/e2e/todos.cy.ts
+++ 06-e2e-testing-102/cypress/e2e/todos.cy.ts
@@ -1,5 +1,13 @@
 describe("Firebase Todos", () => {
-	it("passes", () => {
-		cy.visit("/");
+	context("Can't access protected routes without authenticating first", () => {
+		it("Should redirect to login-page when trying to access todos", () => {
+			cy.visit("/todos");
+
+			// Check that URL is not /todos
+			cy.location("pathname").should("not.equal", "/todos");
+
+			// Check that URL is /login
+			cy.location("pathname").should("equal", "/login");
+		});
 	});
 });
